d9906f601efff56bc2c1e445266ba1df64abade8
ljurak <lucas.jurak@gmail.com>
2020-06-13 16:00:50 +0200

    Add identifier property

--- src/main/java/blockchain/ChatMessage.java
+++ src/main/java/blockchain/ChatMessage.java
@@ -2,16 +2,23 @@ package blockchain;
 
 public class ChatMessage {
 
+    private final long id;
+
     private final String message;
 
     private final String username;
 
     private final byte[] signature;
 
-    public ChatMessage(String message, String username, byte[] signature) {
+    public ChatMessage(long id, String message, String username, byte[] signature) {
+        this.id = id;
         this.message = message;
         this.username = username;
         this.signature = signature;
+    }
+
+    public long getId() {
+        return id;
     }
 
     public String getMessage() {
