df25ccda9a341ac2c8d7724fac4df590b75e4f72
ljurak <lucas.jurak@gmail.com>
2020-06-17 03:05:27 +0200

    Add methods for managing amount of coins

--- src/main/java/blockchain/Miner.java
+++ src/main/java/blockchain/Miner.java
@@ -1,5 +1,9 @@
 package blockchain;
 
 public interface Miner {
+    int getMinerId();
+    int getCoins();
+    void addCoins(int coins);
+    void removeCoins(int coins);
     void mineNextBlock();
 }
